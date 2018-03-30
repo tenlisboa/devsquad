5ff6a1c3175ce8acbd4a59e22e86e3bcb6ad1090
Gabriel Bento Lisboa <gabrielbento.rx@outlook.com>
2018-03-29 23:36:36 -0300

    Making the components VUEjs
    Makiing the initial layouts with Vue.

---
+++ resources/assets/js/components/TaskComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="col-sm-4">
+        <div class="card card-success" >
+            <div class="card-header" >
+                {{ nameTask }}
+            </div>
+            <div class="card-body" >
+
+            </div>
+        </div>
+    </div>                     
+</template>
+
+<script>
+    export default {
+        props: ['nameTask']
+    }
+</script>
